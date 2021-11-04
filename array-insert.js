cccc0f5a3d180fe10cde25a14fcdb8b2bf0241d6
Joshua Kordek <79750252+joshkordek3@users.noreply.github.com>
2021-11-03 19:45:15 -0500

    Delete array-insert.js

--- array-insert.js
+++
@@ -1,9 +0,0 @@
-class ArrayExtension extends Array {
-  insert (item, index) {
-    this.splice(index, 0, item);
-  }
-}
-Array.prototype.insert = function (item, index) {
-  this.splice(index, 0, item);
-  return this;
-}
