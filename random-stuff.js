49fb2f62daa223a50ed89842eaa71e32a1028853
Joshua Kordek <79750252+joshkordek3@users.noreply.github.com>
2021-11-03 19:27:51 -0500

    Update random-stuff.js

--- random-stuff.js
+++ random-stuff.js
@@ -1,12 +1,12 @@
-const objectToArray = (object, containKeys) => (Object.keys(object).map((key) => containKeys ? [key, object[key]] : object[key]));
-const convertToString = (thing) => (typeof thing === 'function' ? String(thing) : JSON.stringify(thing));
-const checkType = (val) => {
+export const objectToArray = (object, containKeys) => (Object.keys(object).map((key) => containKeys ? [key, object[key]] : object[key]));
+export const convertToString = (thing) => (typeof thing === 'function' ? String(thing) : JSON.stringify(thing));
+export const checkType = (val) => {
   const valType = typeof val;
   if (valType != "string") throwErrors("expected type of val to be a string but instead got a" + checkType(valType));
   const vowels = ["a", "e", "i", "o", "u"];
   return (vowels.indexOf(val[0]) == -1 ? " " : "n ") + val;
 };
-const throwErrors = (errors) => {
+export const throwErrors = (errors) => {
   const errorsType = typeof errors;
   const stringified = JSON.stringify(errors);
   if (!(Array.isArray(errors) || errorsType == "string")) throw new Error("expected type of errors to be a string or an array but instead got a" + checkType(errorsType));
